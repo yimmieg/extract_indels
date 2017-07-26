88b9e8179dfc3ffd532c858d7b84b0544190265a
yimmieg <yimmieg@gmail.com>
2017-07-25 23:46:25 -0700

    fixed indentations again

--- extract_indels.py
+++ extract_indels.py
@@ -13,16 +13,16 @@ def extract_reads(options):
             for (cigarType,cigarLength) in cigarLine:
                try:
                    if(cigarType == 0): #match
-                    elif(cigarType == 1 or cigarType == 2): #insertions
+                   elif(cigarType == 1 or cigarType == 2): #insertions
                         mate <- bamfile.mate(read)
                         out.write(read)
                         out.write(mate)
                     ##elif(cigarType == 2): #deletion
-                    elif(cigarType == 3): #skip
-                    elif(cigarType == 4): #soft clipping
-                    elif(cigarType == 5): #hard clipping
-                    elif(cigarType == 6): #padding
-                    else:
+                   elif(cigarType == 3): #skip
+                   elif(cigarType == 4): #soft clipping
+                   elif(cigarType == 5): #hard clipping
+                   elif(cigarType == 6): #padding
+                   else:
                        print "Wrong CIGAR number";
                        sys.exit(1);
                except:

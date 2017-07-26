9304a59013c8f01fde4e5b4fea5d4fe30db3d0c4
yimmieg <yimmieg@gmail.com>
2017-07-26 01:13:45 -0700

    fixed indentation

--- extract_indels.py
+++ extract_indels.py
@@ -10,9 +10,9 @@ def extract_reads(options):
             if( not( read.is_unmapped ) and not (read.mate_is_unmapped ) ):
                 if(read.cigarstring.find("I") != -1 or read.cigarstring.find("D") != -1):
                     mate = bamfile.mate(read)
-                        if (read.tid == mate.tid) :
-                            out.write(read)
-                            out.write(mate)
+                    if (read.tid == mate.tid) :
+                        out.write(read)
+                        out.write(mate)
 
     bamfile.close()
     out.close()

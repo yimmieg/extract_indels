558ac9f24c5005c782c9127e5331a0649141311b
yimmieg <yimmieg@gmail.com>
2017-07-25 23:39:06 -0700

    looks good

--- extract_indels.py
+++ extract_indels.py
@@ -3,18 +3,21 @@ import pysam
 def extract_reads(options):
     n = get_names(options.names)
     bamfile = pysam.AlignmentFile(options.bam, 'rb')
+    header = bamfile.header.copy()
+    out = pysam.Samfile(options.out, 'wb', header=header)
 
     for read in bamfile.fetch():
-     if read.is_paired:
-         if( not( read.is_unmapped ) ):   #if it's mapped
-            cigarLine=read.cigar;
+        if read.is_paired:
+            if( not( read.is_unmapped ) ):   #if it's mapped
+                cigarLine=read.cigar;
             for (cigarType,cigarLength) in cigarLine:
                try:
-                   if(  cigarType == 0): #match
-               elif(cigarType == 1 || cigarType == 2): #insertions
-                mate <- bamfile.mate(read)
-               .write(read)
-                   elif(cigarType == 2): #deletion
+                   if(cigarType == 0): #match
+                    elif(cigarType == 1 || cigarType == 2): #insertions
+                     mate <- bamfile.mate(read)
+                     out.write(read)
+                     out.write(mate)
+                    ##elif(cigarType == 2): #deletion
                    elif(cigarType == 3): #skip
                    elif(cigarType == 4): #soft clipping
                    elif(cigarType == 5): #hard clipping
@@ -25,39 +28,8 @@ def extract_reads(options):
                except:
                    print "Problem";
 
-         if
-             pairedreads.write(read)
-
-    pairedreads.close()
-
-    name_indexed = pysam.IndexedReads(bamfile)
-    name_indexed.build()
-    header = bamfile.header.copy()
-    out = pysam.Samfile(options.out, 'wb', header=header)
-    for name in n:
-        try:
-            name_indexed.find(name)
-        except KeyError:
-            pass
-        else:
-            iterator = name_indexed.find(name)
-            for x in iterator:
-                out.write(x)
-
-
-
-
-for read in samfile.fetch():
-     if read.is_paired:
-             pairedreads.write(read)
-
-pairedreads.close()
-samfile.close()
-
-for read in samfile.fetch('chr1', 100, 120):
-     print read
-
-samfile.close()
+bamfile.close()
+out.close()
 
 if __name__ == "__main__":
     from argparse import ArgumentParser

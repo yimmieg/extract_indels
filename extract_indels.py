0a1129f32a4f8bdfc890dc80020d9a8b1033ade3
yimmieg <yimmieg@gmail.com>
2017-07-26 00:21:39 -0700

    shorted the cigar testing

--- extract_indels.py
+++ extract_indels.py
@@ -8,25 +8,10 @@ def extract_reads(options):
 
     for read in bamfile.fetch():
         if read.is_paired:
-            if( not( read.is_unmapped ) ):   #if it's mapped
-                cigarLine=read.cigar;
-            for (cigarType,cigarLength) in cigarLine:
-               try:
-                   if(cigarType == 0): #match
-                   elif(cigarType == 1 or cigarType == 2): #insertions
-                        mate <- bamfile.mate(read)
-                        out.write(read)
-                        out.write(mate)
-                    ##elif(cigarType == 2): #deletion
-                   elif(cigarType == 3): #skip
-                   elif(cigarType == 4): #soft clipping
-                   elif(cigarType == 5): #hard clipping
-                   elif(cigarType == 6): #padding
-                   else:
-                       print "Wrong CIGAR number";
-                       sys.exit(1);
-               except:
-                   print "Problem";
+            if(read.cigarstring.find("I") or read.cigarstring.find("D")):
+                mate <- bamfile.mate(read)
+                out.write(read)
+                out.write(mate)
 
 bamfile.close()
 out.close()
